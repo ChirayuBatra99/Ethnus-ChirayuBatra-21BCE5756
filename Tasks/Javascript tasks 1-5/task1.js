0edceb5f3030962743ebdbe28e974ff2f09665c7
chirayubatra1104 <chirayubatra1104@gmail.com>
2024-06-15 12:12:16 +0530

    Adding JS tasks all

---
+++ Tasks/Javascript tasks 1-5/task1.js
@@ -0,0 +1,8 @@
+function reverseNumber(number) {
+    let numberStr = number.toString();
+    let reversedStr = numberStr.split('').reverse().join('');
+    let reversedNumber = parseFloat(reversedStr) * Math.sign(number);
+    return reversedNumber;
+}
+
+
